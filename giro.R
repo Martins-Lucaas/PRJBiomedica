b97e53bd7424fbb3c390660f9671888894d32406
Lucas_Martins <lucaspmartins14@gmail.com>
2024-02-17 15:26:35 -0300

    FirstCommit

---
+++ giro.R
@@ -0,0 +1,6 @@
+# Recebendo o sinal -------------------------------------------------------
+setwd("C:/Users/lucas/OneDrive/Documentos/Faculdade/7 Semestre/Proj")
+dados <- read.table("giro.txt",sep="\t",header=T)
+
+
+
